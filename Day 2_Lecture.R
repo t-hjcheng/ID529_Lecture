1ed040dac4d57076d4bd59c47f13bcee2e4c4be8
unknown <thjcheng@gmail.com>
2026-01-13 15:28:01 -0500

    First try

---
+++ Day 2_Lecture.R
@@ -0,0 +1,16 @@
+
+# 2.1 ---------------------------------------------------------------------
+# install devtools if you haven't already
+install.packages("devtools") 
+
+# install our ID529tutorials package
+devtools::install_github("ID529/ID529tutorials")
+
+library(ID529tutorials)
+available_tutorials('ID529tutorials')
+run_tutorial('logic', 'ID529tutorials')
+
+
+# 2.3 ---------------------------------------------------------------------
+
+
