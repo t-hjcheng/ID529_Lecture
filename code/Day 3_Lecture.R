4da40dc0790e9d80ce69ce1c0df2d12bd67b4893
unknown <thjcheng@gmail.com>
2026-01-14 13:59:33 -0500

    Day 3

--- code/Day 3_Lecture.R
+++ code/Day 3_Lecture.R
@@ -1,5 +1,6 @@
 # 3.1 ---------------------------------------------------------------------
-library(tidyverse)    #用tidyverse 這個package
+library(tidyverse)    #用tidyverse這個package
+
 # Print first 10 rows
 print(our_data, n = 10)
 # Take our data, which is stored as a tibble
@@ -8,5 +9,20 @@ new_data <- our_data |>
   filter(group == "i <3 dplyr") |>
   # THEN arrange the result by number of gold stars
   arrange(desc(num_gold_stars))
+
+glimpse(data, width = 50)
+# Explicitly rename variables in the dataset
+rename(data, 
+       sbp = mean_BP,
+       pov_ratio = poverty_ratio,
+       race_eth = race_ethnicity,
+) |> 
+  glimpse()
+
+
+
+
+
+
 
 
