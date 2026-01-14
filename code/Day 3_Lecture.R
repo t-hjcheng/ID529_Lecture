8ba1b1b036d675cc5895b27c5c54d6334941f491
unknown <thjcheng@gmail.com>
2026-01-14 15:19:00 -0500

    Day 3

--- code/Day 3_Lecture.R
+++ code/Day 3_Lecture.R
@@ -1,16 +1,21 @@
 # 3.1 ---------------------------------------------------------------------
 library(tidyverse)    #用tidyverse這個package
+# 如果檔名結尾是 .rda
+load("C:/Users/Cheng/Desktop/Harvard/2025 Spring/ID 529 Data Management and Analytic Workflows in R/ID529_Lecture/data/Day 3/nhanes_id529.rda")
+#賦值並轉換為 tibble 格式 (關鍵步驟！)
+our_data <- as_tibble(nhanes_id529)
 
 # Print first 10 rows
 print(our_data, n = 10)
 # Take our data, which is stored as a tibble
-new_data <- our_data |>
+data <- our_data |>
   # THEN filter our data for a subset of rows
   filter(group == "i <3 dplyr") |>
   # THEN arrange the result by number of gold stars
   arrange(desc(num_gold_stars))
 
 glimpse(data, width = 50)
+
 # Explicitly rename variables in the dataset
 rename(data, 
        sbp = mean_BP,
@@ -18,6 +23,17 @@ rename(data,
        race_eth = race_ethnicity,
 ) |> 
   glimpse()
+view(data)
+
+
+# 3.2 ---------------------------------------------------------------------
+
+#????
+
+
+# 3.3 ---------------------------------------------------------------------
+
+
 
 
 
