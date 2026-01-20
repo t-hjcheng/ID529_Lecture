0119ea9f55170d8f7b97bfaafb69174773099f93
unknown <thjcheng@gmail.com>
2026-01-20 14:53:34 -0500

    Day 6

---
+++ code/Day 6_Lecture.R
@@ -0,0 +1,36 @@
+install.packages("MetBrewer")
+
+install.packages("devtools")
+devtools::install_github("BlakeRMills/MetBrewer")
+
+# 6.1 ---------------------------------------------------------------------
+
+# Install R Markdown (if you hav1e not already done so)
+install.packages("rmarkdown")
+
+install.packages("tinytex")
+tinytex::install_tinytex()
+# to uninstall TinyTeX, run tinytex::uninstall_tinytex() 
+
+#see the example: Day 6 my_reproducible_report.Rmd  !!!
+
+#see the other example: Day 6 id529_day6_regression_models.Rmd !!!
+
+
+#  USE # to establish tittiles
+#  Use "Ctrl + Alt + i" hot key to start a code chunk
+
+
+
+# 6.2 ---------------------------------------------------------------------
+
+
+
+
+
+
+
+
+# 6.2 ---------------------------------------------------------------------
+
+
